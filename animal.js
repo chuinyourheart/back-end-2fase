a1b6119eee7f97aca5ce062772a9afa150abd506
mio <isabela.oliveira.maria@escola.pr.gov.br>
2026-06-03 13:53:31 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,1 +1,10 @@
+const animal = {
+    nome: "Mushu",  //string
+    especie: "Dragão",  //string
+    idade: 817,  //number
+    tutor: false,  //boolean
+    vacinado: true  //boolean
+};
 
+//console.log(animal);
+console.log(animal.nome);
